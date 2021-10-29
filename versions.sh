55675f166920543816b2ae3f25d6b4919b944367
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2021-10-28 22:25:27 -0700

    feat: all kernel versions since v4.0
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

---
+++ versions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+
+output="versions.txt"
+owner="torvalds"
+repo="linux"
+per_page=100
+page=1
+
+true > "${output}"
+
+while true; do
+  url="https://api.github.com/repos/${owner}/${repo}/tags?per_page=${per_page}&page=${page}"
+  res=$(curl "${url}" | jq -r '.[].name | select(. | test("^v[4-9].[0-9]+$"))')
+  if [ -z "${res}" ]; then
+    break
+  fi
+  echo "${res}" >> "${output}"
+  page=$((page + 1))
+done
